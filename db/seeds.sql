fe13ef62d2d9f7e5934467f6d499c5eb9555fc74
Tomas Utreras <tutreras100@gmail.com>
2021-01-09 16:54:37 -0500

    SQL

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,12 @@
 USE burgers_db;
 
+INSERT INTO burgers (burger_name) VALUES ("Burguer 1");
+
+INSERT INTO burgers (burger_name) VALUES ("Burguer 2");
+
+INSERT INTO burgers (burger_name) VALUES ("Burguer 3");
+
+INSERT INTO burgers (burger_name) VALUES ("Burguer 4");
+
+INSERT INTO burgers (burger_name) VALUES ("Burguer 5");
+
